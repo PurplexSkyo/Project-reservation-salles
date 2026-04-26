e7bcbc293a3eb3ca8a8e01a995d7101856df35ba
PurplexSkyo <npoulain678@gmail.com>
2026-04-26 23:59:34 +0200

    Changement du lien pour detailssalle

--- index.php
+++ index.php
@@ -68,7 +68,7 @@ require_once 'config/header.php';
                             <p class="info-prix"><strong>€</strong> <?= number_format($salle['prix'], 0) ?>€ / h</p>
                         </div>
                         <div class="actions">
-                            <a href="DetailsSalle.php?id=<?= (int)$salle['id_salle'] ?>" class="btn-voir">Voir la salle ⩥</a>
+                            <a href="DetailSalle.php?id=<?= (int)$salle['id_salle'] ?>" class="btn-voir">Voir la salle ⩥</a>
                             <a href="Reservation.php?id=<?= (int)$salle['id_salle'] ?>" class="btn-reserver">Réserver</a>
                         </div>
                     </div>
